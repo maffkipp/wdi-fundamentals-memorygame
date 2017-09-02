c3ca9c77799a5ced00ae99f54a6423319975cc64
Wesley Maffly-Kipp <maffly@MacBook-Pro.local>
2017-09-02 17:40:17 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,11 +30,12 @@ var checkForMatch = function () {
 	}
 }
 
-var flipCard = function (cardId) {
-
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id')
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
@@ -42,5 +43,14 @@ var flipCard = function (cardId) {
 	console.log(cards[cardId].cardImage);
 }
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+	for (var i=0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
